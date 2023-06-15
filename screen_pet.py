b2241e6f750472f443021a7037460e14fd07da8d
mithulanm <134401634+mithulanm@users.noreply.github.com>
2023-06-15 04:09:06 -0700

    Add files via upload

--- screen_pet.py
+++ screen_pet.py
@@ -4,7 +4,7 @@ def toggle_eyes():
     new_color = c.body_color if current_color == 'white' else 'white'
     current_state = c.itemcget(pupil_left, 'state')
     new_state = NORMAL if current_state == HIDDEN else HIDDEN
-    c.itemconfigure(pupil_left, state=new_state)
+    c.itemconfigure(pupil_left, state= new_state)
     c.itemconfigure(pupil_right, state=new_state)
     c.itemconfigure(eye_left, fill=new_color)
     c.itemconfigure(eye_right, fill=new_color)
@@ -18,12 +18,10 @@ root=Tk()
 c=Canvas(root, width=400, height=400)
 c.configure(bg='dark blue',highlightthickness=0)
 
-c.body_color = 'SkyBlue1'
+c.body_color = 'skyBlue1'
 body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
 ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
-ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, \
-                             fill=c.body_color)
-
+ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
 foot_left = c.create_oval(65, 320, 145, 360, outline=c.body_color, fill= c.body_color)
 foot_right = c.create_oval(250, 320, 330, 360, outline=c.body_color, fill= c.body_color)
 eye_left = c.create_oval(130, 110, 160, 170, outline='black', fill='white')
@@ -37,7 +35,9 @@ mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2, state
 cheek_left = c.create_oval(70, 180, 120, 230, outline='pink', fill='pink', state=HIDDEN)
 cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink', state=HIDDEN)
 
+
 c.pack()
+
 
 root.after(1000, blink)
 def show_happy(event):
@@ -58,6 +58,3 @@ def hide_happy(event):
 c.bind('<Motion>', show_happy)
 c.bind('<Leave>', hide_happy)
 root.mainloop()
-
-
-
